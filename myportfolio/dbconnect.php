882237f65ca504742b1d78e7162c464433125491
sndhabilak <111198584+sndhabilak@users.noreply.github.com>
2024-02-04 14:54:18 +0100

    Add files via upload

---
+++ myportfolio/dbconnect.php
@@ -0,0 +1,10 @@
+<?php
+// Connects to your Database 
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "portfolio";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+?>
